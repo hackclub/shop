daadea74e1330e6240ea065c6cce3166b889eec5
merelinguist <merelinguist@users.noreply.github.com>
2018-08-11 16:20:56 +0100

    Add Footer

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# store
+# shop
 
 **Work in progress**
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -2,16 +2,15 @@ import React, { Fragment } from 'react'
 import { ThemeProvider } from '@hackclub/design-system'
 
 import { Container, InnerContainer } from './style'
-import Footer from '../Footer/index.js'
+import Footer from '../Footer'
 
 export default props => (
   <ThemeProvider webfonts>
     <Container>
       <InnerContainer>
-        <Fragment>
-          {props.children}
-        </Fragment>
+        <Fragment>{props.children}</Fragment>
       </InnerContainer>
+      <Footer />
     </Container>
   </ThemeProvider>
 )
