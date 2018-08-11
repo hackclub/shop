9e6e5c54db0f476f513b3a5c49e5f8896ba7e8d7
merelinguist <merelinguist@users.noreply.github.com>
2018-08-11 19:22:37 +0100

    Product pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,64 @@
 // implement node api stuff here
 
+const path = require('path')
+
+const toKebabCase = str =>
+  str &&
+  str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map(x => x.toLowerCase())
+    .join('-')
+
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === 'ShopifyProducts') {
+
+    // shortcut to getting the featured image
     const image = node.images.edges[0].node.src
     createNodeField({
       node,
       name: 'image',
       value: image
     })
+
+    // slugify the title
+    const slug = toKebabCase(node.title)
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug
+    })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allShopifyProducts {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      result.data.allShopifyProducts.edges.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve('./src/templates/product.js'),
+          context: {
+            // Data passed to context is available
+            // in page queries as GraphQL variables.
+            slug: node.fields.slug,
+          },
+        })
+      })
+      resolve()
+    })
+  })
 }
---
+++ src/templates/product.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default p => <h1>hello world</h1>
