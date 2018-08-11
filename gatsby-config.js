34eedeb5447dce5d426d3e7a85e8048169be7b0e
merelinguist <merelinguist@users.noreply.github.com>
2018-08-11 17:33:33 +0100

    Add shopify source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
 // configure plugins here
 module.exports = {
   plugins: [
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-shopify-storefront',
+      options: {
+        siteName: 'hackclub',
+        accessToken: 'ba720b36f1e99b2719bc74ef728ec847'
+      },
+    }
   ]
 }
