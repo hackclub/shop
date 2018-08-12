76d28349a387ada901ffefb135dda468ef74f43f
merelinguist <merelinguist@users.noreply.github.com>
2018-08-12 02:27:13 +0100

    Add scroll to top button

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,17 +1,56 @@
-import React from 'react'
-import { Container, ThemeProvider } from '@hackclub/design-system'
+import React, { Component } from 'react'
+import { Container, Icon, ThemeProvider } from '@hackclub/design-system'
 import Helmet from 'react-helmet'
+import { throttle } from 'throttle-debounce'
 
 import Transition from '../Transition'
+import { ScrollToTop } from './style'
 export { Gradient, SectionHeading } from './style'
 
-export default props => (
-  <ThemeProvider webfonts>
-    <Helmet>
-      <title>Hack Club Store</title>
-    </Helmet>
-    <Container px={3}>
-      <Transition>{props.children}</Transition>
-    </Container>
-  </ThemeProvider>
-)
+export default class extends Component {
+  constructor() {
+    super()
+
+    this.state = { scrollToTopVisible: false }
+    this.handleScroll = throttle(300, this.handleScroll)
+  }
+
+  componentDidMount() {
+    window && window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window && window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const scrollToTopVisible = window && window.pageYOffset > 240
+    return this.setState({ scrollToTopVisible })
+  }
+
+  scrollToTop = () => {
+    return window && window.scrollTo(0, 0)
+  }
+
+  render() {
+    const { children } = this.props
+    const { scrollToTopVisible } = this.state
+
+    return (
+      <ThemeProvider webfonts>
+        <Helmet>
+          <title>Hack Club Store</title>
+        </Helmet>
+        <Container px={3}>
+          <Transition>{children}</Transition>
+          <ScrollToTop
+            isVisible={scrollToTopVisible}
+            onClick={this.scrollToTop}
+            name="keyboard_arrow_up"
+            size={32}
+          />
+        </Container>
+      </ThemeProvider>
+    )
+  }
+}
--- src/components/Page/style.js
+++ src/components/Page/style.js
@@ -1,4 +1,5 @@
-import { Box, Heading } from '@hackclub/design-system'
+import styled from 'styled-components'
+import { Box, Heading, IconButton } from '@hackclub/design-system'
 
 export const SectionHeading = Heading.h2.extend.attrs({
   f: [4, 5],
@@ -13,4 +14,45 @@ export const Gradient = Box.extend`
     ${({ theme }) => theme.colors.white},
     ${({ theme }) => theme.colors.snow}
   );
+`
+
+export const ScrollToTop = styled(IconButton)`
+  width: 48px;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  bottom: 32px;
+  right: 32px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.08);
+  transition: all 0.2s ease-in-out;
+  opacity: ${props => (props.isVisible ? '1' : '0')};
+  background-color: rgb(228, 45, 66);
+  background-image: linear-gradient(
+    90deg,
+    rgb(228, 115, 45),
+    rgb(228, 45, 66),
+    rgb(206, 41, 60)
+  );
+  color: ${props => props.theme.colors.white};
+  transform: translateY(${props => (props.isVisible ? '0' : '80px')});
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 8px 24px rgba(0, 0, 0, 0.1);
+    transform: translateY(-3px);
+    transition: all 0.2s ease-in-out;
+  }
+  &:active {
+    box-shadow: 0 6px 20px rgba(0, 0, 0, 0.09);
+    transform: translateY(-2px);
+    transition: all 0.2s ease-in-out;
+  }
+  .icon {
+    transform: rotate(270deg);
+  }
+  @media (max-width: 968px) {
+    bottom: 16px;
+    right: 16px;
+  }
 `
