6d04ae9f6129ac3512858c44a70b613197a937a3
Zane <github@zane.sh>
2018-08-12 21:30:14 -0700

    Add <ImageLoader /> component

---
+++ src/components/ImageLoader/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Loading } from '@hackclub/design-system'
+import { LoadingBase } from './style'
+
+export default () => (
+  <LoadingBase>
+    <Loading />
+  </LoadingBase>
+)
---
+++ src/components/ImageLoader/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { Flex } from '@hackclub/design-system'
+
+export const LoadingBase = styled(Flex)`
+  height: 0;
+  position: relative;
+  padding-bottom: 100%;
+  flex-direction: column;
+  justify-content: center;
+`
