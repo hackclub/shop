483acc33371883a9cd1619651ec99c26f1faecde
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-08-19 17:29:10 -0400

    She's a real cutie

--- src/components/ProductImage/style.js
+++ src/components/ProductImage/style.js
@@ -2,12 +2,13 @@ import styled from 'styled-components'
 import Img from 'react-image'
 
 export const BlurredArt = styled(Img)`
-  border-radius: 8px;
+  border-radius: ${({ theme }) => theme.radii[2]};
   position: absolute;
-  width: 100%;
-  height: 100%;
+  width: 106.25%;
+  height: 106.25%;
+  left: -3.125%;
   filter: blur(12px);
-  opacity: 0.6;
+  opacity: 0.5;
   z-index: 1;
 
   /* there are some problems with blurring images on mobile devices and perf */
@@ -17,7 +18,7 @@ export const BlurredArt = styled(Img)`
 `
 
 export const Art = styled(Img)`
-  border-radius: 8px;
+  border-radius: ${({ theme }) => theme.radii[2]};
   width: 100%;
   height: 100%;
   margin-bottom: -6px;
