b5164e5a5494a2434d08acbc6d8c313475039f6e
merelinguist <merelinguist@users.noreply.github.com>
2018-08-20 09:54:02 +0100

    Add shimmering image

--- src/components/ImageLoader/index.js
+++ src/components/ImageLoader/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Loading } from '@hackclub/design-system'
-import { LoadingBase } from './style'
+import ContentLoader from 'react-content-loader'
 
-export default () => (
-  <LoadingBase>
-    <Loading />
-  </LoadingBase>
+export default props => (
+  <ContentLoader
+    height={320}
+    width={320}
+    speed={2}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+    {...props}
+  >
+    <rect x="0" y="0" rx="5" ry="5" width="320" height="320" />
+  </ContentLoader>
 )
