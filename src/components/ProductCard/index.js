65a035f678b057aa10209dabbf741494eb528827
Zane <github@zane.sh>
2018-09-02 14:49:05 -0700

    Differentiate out of stock items in product grid

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -11,6 +11,7 @@ export default ({
     fields: { slug, image },
     title,
     description,
+    availableForSale,
     variants
   }
 }) => (
@@ -23,17 +24,23 @@ export default ({
         </FadeIn>
       </ImageWrapper>
       <Box p={3}>
-        <Heading.h3 f={[3, 4]} mb={1}>
+        <Heading.h3
+          f={[3, 4]}
+          mb={1}
+          color={availableForSale ? 'black' : 'muted'}
+        >
           {title}
         </Heading.h3>
-        <Text f={2} mb={2}>
+        <Text f={2} mb={2} color={availableForSale ? 'black' : 'muted'}>
           {
             JSON.parse(description)
               .description /* parse description from JSON */
           }
         </Text>
         <Text f={2} color="muted">
-          ${variants.edges[0].node.price}
+          {availableForSale
+            ? `$${variants.edges[0].node.price}`
+            : 'Out of Stock'}
         </Text>
       </Box>
     </Item>
--- src/components/ProductsGrid/index.js
+++ src/components/ProductsGrid/index.js
@@ -14,6 +14,7 @@ export default () => (
               id
               title
               description
+              availableForSale
               variants {
                 edges {
                   node {
