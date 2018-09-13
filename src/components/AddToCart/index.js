dbdfea6a34d7d30c31853a5e8c2ac9acda8d7444
Zane <github@zane.sh>
2018-09-12 18:37:10 -0700

    Split into separate components

--- src/components/AddToCart/index.js
+++ src/components/AddToCart/index.js
@@ -10,136 +10,143 @@ export default class extends Component {
     added: false
   }
 
+  setAdded = () => {
+    this.setState({
+      added: true
+    })
+  }
+
   render() {
-    const { variants, availableForSale, exists } = this.props
+    const { exists } = this.props
     const { added } = this.state
     return exists ? (
-      <StoreContext.Consumer>
-        {({ addVariantToCart }) => (
-          <Formik
-            initialValues={{
-              variant:
-                variants.edges.length === 1
-                  ? variants.edges[0].node
-                      .id /* if there’s only one variant, pre-fill that ID */
-                  : '',
-              quantity: 1
-            }}
-            validationSchema={yup.object().shape({
-              variant: yup.string().required(),
-              quantity: yup
-                .number()
-                .required()
-                .min(1, 'must be at least 1')
-            })}
-            onSubmit={values => {
-              addVariantToCart(values.variant, values.quantity)
-              this.setState({
-                added: true
-              })
-            }}
-          >
-            {({ values, errors, handleChange, handleSubmit }) => (
-              <>
-                {variants.edges.length !== 1 && (
-                  <Field
-                    name="variant"
-                    value={values.variant}
-                    label="Choose a size"
-                    onChange={handleChange}
-                    type="select"
-                    error={errors.variant}
-                  >
-                    {variants.edges.map(variant => (
-                      <option value={variant.node.id} key={variant.node.id}>
-                        {variant.node.title}
-                      </option>
-                    ))}
-                  </Field>
-                )}
-                <Field
-                  name="quantity"
-                  value={values.quantity}
-                  label="Choose a quantity"
-                  onChange={handleChange}
-                  type="number"
-                  error={errors.quantity}
-                />
-                <IconButton
-                  onClick={
-                    availableForSale
-                      ? added
-                        ? undefined
-                        : handleSubmit
-                      : undefined
-                  }
-                  type="submit"
-                  mt={3}
-                  size={32}
-                  bg={
-                    availableForSale ? (added ? 'success' : 'primary') : 'muted'
-                  }
-                  glyph="bag-add"
-                  is={LargeButton}
-                >
-                  {availableForSale
-                    ? added
-                      ? 'Added'
-                      : 'Add to Bag'
-                    : 'Out of Stock'}
-                </IconButton>
-              </>
-            )}
-          </Formik>
-        )}
-      </StoreContext.Consumer>
+      <InStockProduct added={added} setAdded={this.setAdded} {...this.props} />
     ) : (
-      <>
-        <Text f={3} mb={2}>
-          This product is in trial and hasn’t been produced yet—you can signup
-          below if you’re interested.
-        </Text>
-        <Formik
-          initialValues={{ email: '' }}
-          validationSchema={yup.object().shape({
-            email: yup
-              .string()
-              .required()
-              .email()
-          })}
-          onSubmit={values => {
-            console.log(values)
-            // TODO: SUBMIT DATA
-            this.setState({
-              added: true
-            })
-          }}
-        >
-          {({ values, errors, handleChange, handleSubmit }) => (
-            <>
-              <Field
-                name="email"
-                value={values.email}
-                label="Enter your email address"
-                onChange={handleChange}
-                type="email"
-                error={errors.email}
-                placeholder="you@email.com"
-              />
-              <IconButton
-                onClick={added ? undefined : handleSubmit}
-                mt={3}
-                size={32}
-                bg={added ? 'success' : 'primary'}
-                glyph="bag-add"
-                is={LargeButton}
-              >
-                {added ? 'Signed up' : 'I’m interested'}
-              </IconButton>
-            </>
-          )}
-        </Formik>
-      </>
+      <TrialProduct added={added} setAdded={this.setAdded} {...this.props} />
     )
   }
 }
+
+const InStockProduct = ({ added, variants, availableForSale, setAdded }) => (
+  <StoreContext.Consumer>
+    {({ addVariantToCart }) => (
+      <Formik
+        initialValues={{
+          variant:
+            variants.edges.length === 1
+              ? variants.edges[0].node
+                  .id /* if there’s only one variant, pre-fill that ID */
+              : '',
+          quantity: 1
+        }}
+        validationSchema={yup.object().shape({
+          variant: yup.string().required(),
+          quantity: yup
+            .number()
+            .required()
+            .min(1, 'must be at least 1')
+        })}
+        onSubmit={values => {
+          addVariantToCart(values.variant, values.quantity)
+          setAdded()
+        }}
+      >
+        {({ values, errors, handleChange, handleSubmit }) => (
+          <>
+            {variants.edges.length !== 1 && (
+              <Field
+                name="variant"
+                value={values.variant}
+                label="Choose a size"
+                onChange={handleChange}
+                type="select"
+                error={errors.variant}
+              >
+                {variants.edges.map(variant => (
+                  <option value={variant.node.id} key={variant.node.id}>
+                    {variant.node.title}
+                  </option>
+                ))}
+              </Field>
+            )}
+            <Field
+              name="quantity"
+              value={values.quantity}
+              label="Choose a quantity"
+              onChange={handleChange}
+              type="number"
+              error={errors.quantity}
+            />
+            <IconButton
+              onClick={
+                availableForSale
+                  ? added
+                    ? undefined
+                    : handleSubmit
+                  : undefined
+              }
+              type="submit"
+              mt={3}
+              size={32}
+              bg={availableForSale ? (added ? 'success' : 'primary') : 'muted'}
+              glyph="bag-add"
+              is={LargeButton}
+            >
+              {availableForSale
+                ? added
+                  ? 'Added'
+                  : 'Add to Bag'
+                : 'Out of Stock'}
+            </IconButton>
+          </>
+        )}
+      </Formik>
+    )}
+  </StoreContext.Consumer>
+)
+
+const TrialProduct = ({ added, setAdded }) => (
+  <>
+    <Text f={3} mb={2}>
+      This product is in trial and hasn’t been produced yet—you can signup below
+      if you’re interested.
+    </Text>
+    <Formik
+      initialValues={{ email: '' }}
+      validationSchema={yup.object().shape({
+        email: yup
+          .string()
+          .required()
+          .email()
+      })}
+      onSubmit={values => {
+        // TODO: SUBMIT DATA
+        setAdded()
+      }}
+    >
+      {({ values, errors, handleChange, handleSubmit }) => (
+        <>
+          <Field
+            name="email"
+            value={values.email}
+            label="Enter your email address"
+            onChange={handleChange}
+            type="email"
+            error={errors.email}
+            placeholder="you@email.com"
+          />
+          <IconButton
+            onClick={added ? undefined : handleSubmit}
+            mt={3}
+            size={32}
+            bg={added ? 'success' : 'primary'}
+            glyph="bag-add"
+            is={LargeButton}
+          >
+            {added ? 'Signed up' : 'I’m interested'}
+          </IconButton>
+        </>
+      )}
+    </Formik>
+  </>
+)
