9f107f6d78666b10019c64e6a33477acea1c0182
Zane <github@zane.sh>
2018-09-16 00:50:56 -0700

    Improve BulkOrderBanner

--- src/components/BulkOrderBanner/index.js
+++ src/components/BulkOrderBanner/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Text, Link, Card } from '@hackclub/design-system'
 
 export default () => (
-  <Card boxShadowSize="sm" p={3} my={3} bg="white">
+  <Card boxShadowSize="md" p={3} my={3} bg="white">
     <Text bold f={3}>
-      Need stickers in bulk for your event?
+      Are you a club leader or need stickers for your event?
     </Text>
     <Text f={2}>
-      Reach out to us:{' '}
-      <Link href="mailto:team@hackclub.com">team@hackclub.com</Link>
+      Reach out to us via{' '}
+      <Link href="mailto:team@hackclub.com">team@hackclub.com</Link> or{' '}
+      <Link href="https://hackclub.slack.com/messages/hq">#hq on Slack</Link>.
     </Text>
   </Card>
 )
